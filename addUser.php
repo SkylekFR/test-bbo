d0e8ce579732c39709f1953bc4918436e2ddb063
Emilien Champion <emilien.champion@etu.univ-lyon1.fr>
2020-01-31 18:07:43 +0100

    v1.2

--- addUser.php
+++ addUser.php
@@ -8,11 +8,11 @@ try{
     $statement = $pdo->prepare(
         'INSERT INTO user(lastname, firstname, age, address, city) 
                VALUES(:lastname, :firstname, :age, :address, :city)');
-    $statement->bindParam('lastname', $_POST['lastname'], PDO::PARAM_STR);
-    $statement->bindParam('firstname', $_POST['firstname'], PDO::PARAM_STR);
-    $statement->bindParam('age', $_POST['age'], PDO::PARAM_INT);
-    $statement->bindParam('address', $_POST['address'], PDO::PARAM_STR);
-    $statement->bindParam('city', $_POST['city'], PDO::PARAM_STR);
+    $statement->bindParam('lastname', htmlspecialchars($_POST['lastname']), PDO::PARAM_STR);
+    $statement->bindParam('firstname', htmlspecialchars($_POST['firstname']), PDO::PARAM_STR);
+    $statement->bindParam('age', htmlspecialchars($_POST['age']), PDO::PARAM_INT);
+    $statement->bindParam('address', htmlspecialchars($_POST['address']), PDO::PARAM_STR);
+    $statement->bindParam('city', htmlspecialchars($_POST['city']), PDO::PARAM_STR);
     $statement->execute();
 }catch(PDOException $e){
     $pdo->rollBack();
